df607c3541fbc6a566bb994c48fda59010e45caa
mtreasa <maliakal.treasa@gmail.com>
2022-07-13 20:32:23 +0530

    first commit

---
+++ src/main/java/com/greatlearning/studentmanagement/Studentmanagement2Application.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studentmanagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Studentmanagement2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/greatlearning/studentmanagement/Studentmanagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studentmanagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
